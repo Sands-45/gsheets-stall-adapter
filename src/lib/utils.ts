901f3726237e9928650f026c36e85fd8f4249f17
Sands-45 <sandsqa@hotmail.com>
2026-01-15 18:26:08 +0200

    fix: Remove Firebase error message replacement, clean file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,5 @@
 export const get_error_message = (error: unknown): string => {
-  const message =
-    error instanceof Error
-      ? error.message.replace(/firebase/gi, "Stall")
-      : "Unknown error";
+  const message = error instanceof Error ? error.message : "Unknown error";
 
   return message;
 };
