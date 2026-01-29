5bf6b199b2c37bddda6fbe760d83cedfd14d4c37
Sands-45 <sandsqa@hotmail.com>
2026-01-29 03:32:33 +0200

    fix: Updating the export to include the classes instead of regions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { products } from "@/services/products.services";
 export { locations } from "@/services/locations.services";
-export { tax_regions } from "@/services/tax-classes.services";
+export { tax_classes } from "@/services/tax-classes.services";
 export { tax_rates } from "@/services/tax-rates.services";
